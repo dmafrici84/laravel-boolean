751aa7a5e66e16e0614eee91167fdce8d6dd2462
Daniele <dmafrici84@gmail.com>
2020-09-18 14:51:01 +0200

    Lc laravel-boolean impostazioni iniziali

---
+++ mylaravel-boolean/resources/views/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.main-layout')
+
+@section('content')
+  <section class="content">
+    <h1>Hello Home</h1>
+  </section>
+@endsection
---
+++ mylaravel-boolean/resources/views/layouts/main-layout.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>laravel-Boolean</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+
+  </head>
+  <body>
+      <div class="container">
+        @include('partials.header')
+
+        @yield('content')
+
+        @include('partials.footer')
+      </div>
+  </body>
+</html>
