2284e1f3a8bcbfe7bf6ecc8d1441f94538ffb3ef
Daniele <dmafrici84@gmail.com>
2020-09-19 17:02:16 +0200

    Lc Laravel-boolean finito main

--- mylaravel-boolean/resources/views/home.blade.php
+++ mylaravel-boolean/resources/views/home.blade.php
@@ -3,36 +3,52 @@
 @section('content')
   @isset($data)
   <section class="content">
-    <h1 id="lunghe">Le Lunghe</h1>
-    <ul id="lista-lunghe" class="pasta">
+    <div class="lunghe-corte">
+      <h1 id="lunghe">LE LUNGHE</h1>
+      <ul id="lista-lunghe" class="pasta">
 
-        @foreach ($data as $values)
-          @if($values['tipo'] == "lunga")
-            <li> <img src="{{ $values["src"] }}" alt=""></li>
-          @endif
-        @endforeach
+          @foreach ($data as $values)
+            @if($values['tipo'] == "lunga")
+              <li>
+                <a href="#">
+                  <img src="{{ $values["src"] }}" alt="">
+                </a>
+              </li>
+            @endif
+          @endforeach
 
-    </ul>
-    <h1 id="corte">Le Corte</h1>
-    <ul id="lista-corte" class="pasta">
+      </ul>
+      <h1 id="corte">LE CORTE</h1>
+      <ul id="lista-corte" class="pasta">
 
-        @foreach ($data as $values)
-          @if($values['tipo'] == "corta")
-            <li> <img src="{{ $values["src"] }}" alt=""> </li>
-          @endif
-        @endforeach
+          @foreach ($data as $values)
+            @if($values['tipo'] == "corta")
+              <li>
+                <a href="#">
+                  <img src="{{ $values["src"] }}" alt="">
+                </a>
+              </li>
+            @endif
+          @endforeach
 
-    </ul>
-    <h1 id="cortissime">Le Cortissime</h1>
-    <ul id="lista-cortissime" class="pasta">
+      </ul>
+    </div>
+    <div class="cortissime">
+      <h1 id="cortissime">LE CORTISSIME</h1>
+      <ul id="lista-cortissime" class="pasta">
 
-        @foreach ($data as $values)
-          @if($values['tipo'] == "cortissima")
-            <li> <img src="{{ $values["src"] }}" alt=""> </li>
-          @endif
-        @endforeach
+          @foreach ($data as $values)
+            @if($values['tipo'] == "cortissima")
+              <li>
+                <a href="#">
+                  <img src="{{ $values["src"] }}" alt="">
+                </a>
+              </li>
+            @endif
+          @endforeach
 
-    </ul>
+      </ul>
+    </div>
   </section>
   @endisset
   @empty($data)
