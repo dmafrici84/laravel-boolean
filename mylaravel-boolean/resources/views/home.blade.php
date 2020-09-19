60c17a4abe367a2b164156b8982b1893fc2815b6
Daniele <dmafrici84@gmail.com>
2020-09-19 16:30:34 +0200

    Lc laravel-boolean lavori in corso

--- mylaravel-boolean/resources/views/home.blade.php
+++ mylaravel-boolean/resources/views/home.blade.php
@@ -1,7 +1,41 @@
 @extends('layouts.main-layout')
 
 @section('content')
+  @isset($data)
   <section class="content">
-    <h1>Hello Home</h1>
+    <h1 id="lunghe">Le Lunghe</h1>
+    <ul id="lista-lunghe" class="pasta">
+
+        @foreach ($data as $values)
+          @if($values['tipo'] == "lunga")
+            <li> <img src="{{ $values["src"] }}" alt=""></li>
+          @endif
+        @endforeach
+
+    </ul>
+    <h1 id="corte">Le Corte</h1>
+    <ul id="lista-corte" class="pasta">
+
+        @foreach ($data as $values)
+          @if($values['tipo'] == "corta")
+            <li> <img src="{{ $values["src"] }}" alt=""> </li>
+          @endif
+        @endforeach
+
+    </ul>
+    <h1 id="cortissime">Le Cortissime</h1>
+    <ul id="lista-cortissime" class="pasta">
+
+        @foreach ($data as $values)
+          @if($values['tipo'] == "cortissima")
+            <li> <img src="{{ $values["src"] }}" alt=""> </li>
+          @endif
+        @endforeach
+
+    </ul>
   </section>
+  @endisset
+  @empty($data)
+      No from variable
+    @endempty
 @endsection
--- mylaravel-boolean/resources/views/partials/header.blade.php
+++ mylaravel-boolean/resources/views/partials/header.blade.php
@@ -1,3 +1,12 @@
 <header>
-  <h2>I'm my header</h2>
+  <div class="logo">
+    <a href="#">
+      <img src="{{ asset('img/marchio-sito-test.png')}}" alt="">
+    </a>
+  </div>
+  <div class="link">
+    <a href="#">Home</a>
+    <a href="#">Prodotti</a>
+    <a href="#">News</a>
+  </div>
 </header>
