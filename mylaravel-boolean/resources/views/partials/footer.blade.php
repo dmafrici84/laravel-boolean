f13677a39101dbbc1ed3360986e0d2412906587e
Daniele <dmafrici84@gmail.com>
2020-09-21 12:13:50 +0200

    Lc laravel-boolean finito footer

--- mylaravel-boolean/resources/views/partials/footer.blade.php
+++ mylaravel-boolean/resources/views/partials/footer.blade.php
@@ -1,3 +1,67 @@
 <footer>
-  <h2>I'm my footer</h2>
+  <section  class="footer-link">
+    <div class="info-azienda">
+      <a href="#">
+        <img src="{{ asset('img/marchio-sito-test.png')}}" alt="">
+      </a>
+      <ul>
+        <li>Ragione sociale: La
+        Molisana S.P.A.</li>
+        <li>Sede legale:
+        Contrada Colle delle
+        Api, 100/A
+        86100 -
+        Campobasso (CB)</li>
+        <li>Pec:
+        lamolisana@pec.it</li>
+        <li>Tel: +39 0874 4981</li>
+        <li>Fax: +39 0874
+        629584</li>
+        <li>info@lamolisana.it
+        (per segnalazioni
+        degli utenti)</li>
+        <li>commerciale@lamolisana.it</li>
+        <li>export@lamolisana.it</li>
+        <li>numero verde
+        800818081</li>
+        <li>telefono
+        3801292455</li>
+      </ul>
+    </div>
+    <div class="link-utili">
+      <h2>PASTIFICIO</h2>
+      <ul>
+        <li><a href="#">Il Pastificio</a></li>
+        <li><a href="#">Grano decorticato a pietra</a></li>
+        <li><a href="#">Il Molise c’è</a></li>
+        <li><a href="#">Filiera Integrata</a></li>
+        <li><a href="#">100 anni di pasta</a></li>
+        <li><a href="#">Sartoria della pasta</a></li>
+        <li><a href="#">Spaghetto Quadrato</a></li>
+        <li><a href="#">Le Persone</a></li>
+      </ul>
+    </div>
+    <div class="link-utili">
+      <h2>PRODOTTI</h2>
+      <ul>
+        <li><a href="#">Le Classiche</a></li>
+        <li><a href="#">Le Integrali</a></li>
+        <li><a href="#">Le Speciali</a></li>
+        <li><a href="#">Le Biologiche</a></li>
+        <li><a href="#">Le Gluten-Free</a></li>
+        <li><a href="#">Le Semole</a></li>
+        <li><a href="#">Le Extra di Lusso</a></li>
+      </ul>
+    </div>
+    <div class="link-utili">
+      <h2>COLLEZIONE DA CHEF</h2>
+      <ul>
+        <li><a href="#">Collezione da
+        Chef</a></li>
+        <li><a href="#">Grandi Cucine</a></li>
+        <li><a href="#">Biologiche</a></li>
+        <li><a href="#">Quadrate</a></li>
+      </ul>
+    </div>
+  </section>
 </footer>
